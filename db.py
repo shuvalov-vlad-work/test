08be187379fb61656244064a4a82895dcc0dfcc9
shuvalov-vlad-work <vlad.shuvalov.work@ya.ru>
2024-07-20 16:51:33 +0300

    Update db.py: no .env

--- db.py
+++ db.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv() 
 
-MYSQL_USER = os.getenv('MYSQL_USER')
-MYSQL_PSWD = os.getenv('MYSQL_PSWD')
-MYSQL_DB = os.getenv('MYSQL_DB')
+MYSQL_USER = 'root' #os.getenv('MYSQL_USER')
+MYSQL_PSWD = 'password' #os.getenv('MYSQL_PSWD')
+MYSQL_DB = 'test' #os.getenv('MYSQL_DB')
 
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PSWD}@db:3306/{MYSQL_DB}?charset=utf8mb4"
